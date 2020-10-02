e9e884417a9c0a7a98f6e125377b41fd80895b88
sharon0812 <anyangosharon@gmail.com>
2020-10-02 13:01:15 +0300

    modified js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-var maleNames =[]
-var femaleNames =[]
-var weekDays =["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"];
+var maleNames =["Kwasi", "Kwaduo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames =["Akosua", "Aduo", "Abena", "Akua", "Yaa", "Afua", "Ama"];
+var weekDays =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function myFunction(event);
